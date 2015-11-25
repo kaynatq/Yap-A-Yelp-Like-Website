a0245d3eb05418bd9d7f961158fb003ced655222
kaynatq <kquayyum@dons.usfca.edu>
2015-11-24 16:16:38 -0800

    import deleted

--- yapserver/src/yap/servlet/ReviewServlet.java
+++ yapserver/src/yap/servlet/ReviewServlet.java
@@ -1,10 +1,6 @@
 package yap.servlet;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -14,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import yap.data.YapReview;
-import yap.sql.MySQLConnector;
 
 /**
  * Servlet implementation class BusinessServlet
